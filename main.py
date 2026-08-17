e66b509920d69627487d7d921c3979df958ad466
kyle <kaelkibet@gmail.com>
2026-08-17 14:58:37 +0000

    update

--- main.py
+++ main.py
@@ -1,38 +1,49 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
 import random
 import smtplib
+import pandas as pd
+import datetime as dt
 import os
 
-# import os and use it to get the Github repository secrets
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
+GMAIL_SMTP_SERVER = "smtp.gmail.com"
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+def send_email(receiver, msg):
+    with smtplib.SMTP_SSL(GMAIL_SMTP_SERVER, 465) as connection:
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(MY_EMAIL, receiver, msg)
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
-
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
+def generate_letter(name, receiver):
+    letters = ['letter_1.txt', 'letter_2.txt', 'letter_3.txt']
+    letter = random.choice(letters)
+    try:
+        with open(f"letter_templates/{letter}") as f:
+            letter = f.read()
+    except FileNotFoundError:
+        raise FileNotFoundError(f"letter_templates/{letter} not found")
+    else:
+        letter = letter.replace("[NAME]", f"{name}")
+        msg_body = (
+            f"From: {MY_EMAIL}\r\n"
+            f"To: {receiver}\r\n"
+            "MIME-Version: 1.0\r\n"
+            "Content-Type: text/plain; charset=utf-8\r\n"
+            f"Subject: Happy Birthday\r\n"
+            "\r\n"
+            f"{letter}!!!"
         )
+    return msg_body
+
+try:
+    with open("data/birthdays.csv") as data_file:
+        birthdays = pd.read_csv(data_file)
+except FileNotFoundError:
+    print("No birthdays.csv found")
+else:
+    birthdays = birthdays.to_dict(orient="records")
+    now = dt.datetime.now()
+
+    for birthday in birthdays:
+        if now.day == birthday["day"] and now.month == birthday["month"]:
+            message = generate_letter(birthday["name"], birthday["email"])
+            send_email(birthday["email"], message)
